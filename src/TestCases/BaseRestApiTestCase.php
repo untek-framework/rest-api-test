6cf464c8f8507f8c268d98565a0949233f91250c
vitaliy <theyamshikov@yandex.ru>
2023-09-02 16:02:33 +0600

    upd

---
+++ src/TestCases/BaseRestApiTestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Untek\Framework\RestApiTest\TestCases;
+
+use Untek\Framework\WebTest\Libs\JsonImitationRequest;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+abstract class BaseRestApiTestCase extends TestCase
+{
+    protected function getRequestImitator(): JsonImitationRequest
+    {
+        return new JsonImitationRequest($this->endpointScript, $this->baseUrl);
+    }
+
+    protected function sendRequest(?string $uri = null, string $method = 'GET', array $data = []): Response
+    {
+        return $this->getRequestImitator()->sendJsonRequest($uri, $method, $data);
+    }
+}
