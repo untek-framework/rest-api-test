5483e4500b8756e2eb6082aa73599121c84a4f2c
Vitaliy <theyamshikov@yandex.ru>
2024-05-26 20:24:48 +0500

    upd

--- src/Asserts/RestApiResponseAssert.php
+++ src/Asserts/RestApiResponseAssert.php
@@ -47,6 +47,12 @@ class RestApiResponseAssert extends Assert
         return $this;
     }
 
+    public function assertPathTime(string $path = null): static
+    {
+        $this->assertPathWithRegexp('^\d{4}-(?:0[1-9]|1[0-2])-(?:[0-2][1-9]|[1-3]0|3[01])T(?:[0-1][0-9]|2[0-3])(?::[0-6]\d)(?::[0-6]\d)?(?:\.\d{3})?(?:[+-][0-2]\d:[0-5]\d|Z)?$', $path);
+        return $this;
+    }
+
     public function assertPathWithRegexp($exp, string $path = null): static
     {
         $actual = $this->getValueFromPath($path);
